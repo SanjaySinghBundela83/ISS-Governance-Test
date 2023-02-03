eca96dce6f78d768a42770ea38c729f8ed88c114
SanjaySinghBundela83 <86206472+SanjaySinghBundela83@users.noreply.github.com>
2023-02-03 18:11:50 +0530

    Update resize.php

--- resize.php
+++ resize.php
@@ -1,33 +1,25 @@
 function calculateResize($widthA,$heightA,$widthB,$heightB)
 {
 
-        if($widthA != $heightA)
-        {
-            if($widthA > $heightA)
-            {
-                $t_width = $widthB;
-                $t_height = (($t_width * $heightA)/$widthA);
-                //fix height
-                if($t_height > $heightB)
-                {
-                    $t_height = $heightB;
-                    $t_width = (($widthA * $t_height)/$heightA);
-                }
-            }
-            else
-            {
-                $t_height = $heightB;
-                $t_width = (($widthA * $t_height)/$heightA);
-                //fix width
-                if($t_width > $widthB)
-                {
-                    $t_width = $widthB;
-                    $t_height = (($t_width * $heightA)/$widthA);
-                }
-            }
-        }
-        else
-            $t_width = $t_height = min($heightB,$widthB);
+        $o_w = $widthA;
+    $o_h = $heightA;
+    $m_w = $widthB;
+    $m_h = $heightB;
 
-        return array('height'=>(int)$t_height,'width'=>(int)$t_width);
+    $c_w = $m_w / $o_w;
+    $c_h = $m_h / $o_h;
+
+    if($o_w == $o_h){
+            $n_w = ($n_w >= $n_h)?$n_w:$n_h;
+            $n_h = $n_w;
+    } else {
+            if( $c_w > $c_h ) {
+                $n_w = $m_w;
+                $n_h = $o_h * ($n_w/$o_w);
+            } else {
+                $n_h = $m_h;
+                $n_w = $o_w * ($n_h/$o_h);
+            }
+    }
+        return 'height : '.((int)$n_h).' width : '.((int)$n_w);
     }
